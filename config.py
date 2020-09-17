2c123baaa6935ae04976f3f0ffd7e00faea03668
EmilianoHuerta <ehuerta@macalester.edu>
2020-09-17 17:11:25 -0500

    config data, test for now -- will mask this data later

---
+++ config.py
@@ -0,0 +1,12 @@
+keys = {
+        "product_url": "https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434",
+        "first_name": "test name",
+        "last_name": "name",
+        "email": "email@gmail.com",
+        "address": "1995 Thomas Jeff. st",
+        "street_address": "Forest Hills Dr.",
+        "city": "Timbuktu",
+        "zip_code": "91601",
+        "card_cvv": "666",
+        "card_number": "1228199669918221"
+}
